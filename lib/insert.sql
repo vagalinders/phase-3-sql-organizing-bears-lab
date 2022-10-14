b5ea1f4bbb24fd369c708ece87dd199d85f76c91
Chris Mann <chrispmann@gmail.com>
2022-10-13 17:58:28 -0700

    commit tree 8cd0ca2c2650f3c90e1cab7886cf97018c027504

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr Chocolate", 3, "M", "blue", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "F", "green", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 39, "F", "spots", "angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 383, "M", "green", "angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 0, "M", "white", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 35, "M", "black", "angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 593, "M", "yellow", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 2, "M", "stripes", "angry", 0);
